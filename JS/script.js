1b23cdb85929b32e72eeb5b778b94eb51dfc6607
Asem Ibne Zahir <asemibnezahir@gmail.com>
2025-09-04 22:27:34 +0600

    Call Buttons All functionality completed

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+// Blank Array of Object for call history
+const historyArray = [];
 
 // Heart Icon Clicking Function
 const hearts = document.getElementsByClassName('card-awesome');
@@ -11,6 +13,53 @@ for(let heart of hearts)
         setTimeout(() => {
             heart.classList.remove('text-red-600');
         }, 200);
+    })
+}
+
+// Call Button Clicking Function
+const calls = document.getElementsByClassName('js-call');
+for(let call of calls)
+{
+    call.addEventListener('click',function(){
+        const coinNumber = parseInt(document.getElementById('target-coin').innerText);
+        // Handeling coin less than 20 case
+        if(coinNumber<20)
+        {
+            alert('Insuffient coin in your account to call, At least 20 Coin needed');
+            return;
+        }
+
+        // Coin Reducing
+        const finalCoin = coinNumber - 20 ;
+        document.getElementById('target-coin').innerText = finalCoin;
+
+        // Alert Message
+        const serviceName = call.parentNode.parentNode.getElementsByTagName('h1')[0].innerText;
+        const serviceNumber = call.parentNode.parentNode.getElementsByTagName('h2')[0].innerText;
+        alert('Calling ' + serviceName + ' - ' + serviceNumber + '......');
+        
+        // Creating object for history
+        const data = {
+            name: serviceName,
+            number: serviceNumber,
+            date: new Date().toLocaleTimeString()
+        }
+
+        // Completing the history adding
+        const mainContainer = document.getElementById('main-container');
+        const historyContainer = document.createElement('div');
+        historyContainer.classList.add('w-[300px]', 'mx-auto');
+        historyContainer.innerHTML = `
+        <div class="single-history-card flex justify-between items-center p-[12px] bg-[#e7e3e3] rounded-[8px] mb-[10px]">
+            <div class="info">
+                <h3 class="inter-font font-semibold text-[14px]">${data.name}</h3>
+                <h5 class="hind-madurai-font font-normal text-[14px]">${data.number}</h5>
+            </div>
+            <div class="time">
+                <h5 class="hind-madurai-font font-normal text-[12px]">${data.date}</h5>
+            </div>
+        </div>`
+        mainContainer.appendChild(historyContainer);
     })
 }
 
